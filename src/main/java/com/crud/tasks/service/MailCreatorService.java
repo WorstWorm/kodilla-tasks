62cf3f8723d10dd8452cb30e062825bb518674b8
Micha Kaczmarski <miharu.mk@gmail.com>
2023-06-19 21:09:12 +0200

    16. added dynamic mail service by Thymeleaf
     On branch main
    
     Changes to be committed:
    	modified:   src/main/java/com/crud/tasks/config/AdminConfig.java
    	new file:   src/main/java/com/crud/tasks/service/MailCreatorService.java
    	modified:   src/main/java/com/crud/tasks/service/SimpleEmailService.java
    	modified:   src/main/resources/application.properties
    	new file:   src/main/resources/templates/mail/created-trello-card-mail.html

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,31 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+        Context context = new Context();
+        context.setVariable("preview", "You are receiving this message because something has happened.");
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", adminConfig.getCompanyName());
+        context.setVariable("goodbye_msg", "Live long and prosper!");
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+}
